a729dc39feefe4ea141787c149d73ff266740f56
anwer hajji <anwer.hajji@zeta-box.com>
2023-02-21 10:49:57 +0100

    setup project

---
+++ InCallStatusBar/InCallStatusBar/InCallStatusBar.h
@@ -0,0 +1,18 @@
+//
+//  InCallStatusBar.h
+//  InCallStatusBar
+//
+//  Created by Anwar Hajji on 21/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InCallStatusBar.
+FOUNDATION_EXPORT double InCallStatusBarVersionNumber;
+
+//! Project version string for InCallStatusBar.
+FOUNDATION_EXPORT const unsigned char InCallStatusBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InCallStatusBar/PublicHeader.h>
+
+
